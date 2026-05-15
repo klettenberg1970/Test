61f05cc8465dd74754e815f0d799daecfa2a2835
klettenberg1970 <bastenstefan@gmailcom>
2026-05-15 23:07:42 +0200

    4.  Update 15.5

--- Webcomponents/footer.js
+++ Webcomponents/footer.js
@@ -1,26 +1,26 @@
 class Footer extends HTMLElement {
     constructor() {
         super()
-            const shadow = this.attachShadow({ mode: 'open' });
+        const shadow = this.attachShadow({ mode: 'open' });
         
         shadow.innerHTML = /*html*/`
             <style>
-                #footer{
-                    text-align:center;
-                    background-color: #f1f1f1;
-                    color:var(--primary-color);
-                     padding: 1rem;
-                     margin-bottom: 1rem;
+                :host {
+                    display: block;
+                    margin-top: -1rem;  /* Footer nach oben rücken */
                 }
-            
+                
+                #footer {
+                    text-align: center;
+                    background-color: #f1f1f1;
+                    color: var(--primary-color);
+                    padding: 1rem;
+                }
             </style>
            
-            <p id ='footer'> &copy; 2026 Stefan Basten | Köln-Klettenberg</p>
+            <p id='footer'>&copy; 2026 Stefan Basten | Köln-Klettenberg</p>
         `;
     }
-
-
-    
 }
 
 customElements.define('webcomponents-footer', Footer);
