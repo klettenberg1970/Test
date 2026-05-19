1898404ec53964ba582e4a0b66d3f31d739e65a4
klettenberg1970 <bastenstefan@gmailcom>
2026-05-19 15:42:31 +0200

    2. Update 19.05.

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,38 +1,11 @@
 
-import { getTodo, createTodo, deleteTodo } from "./todoRoutes.js";
-
-const container = document.querySelector(".todo-container");
-console.log(container)
+import {handleTodo} from "./handleTodo.js"
+import { todoFormular } from "./formularTodo.js";
 
 const main = async () => {
-    const daten = await getTodo();
-
-    console.log(daten)
-
-    const html = daten.todo.map(todo => {
-        console.log(todo.text);
-        return `
-         <div class="todo-card" data-id="${todo._id}">
-         
-         <p> ${todo.text}</p>
-        
-        <button class="delete-button" >x</button>
-        
-         </div>`;
-    }).join("");
-    console.log(html);
-
-
-    container.innerHTML = html;
-    console.log(container);
-    container.addEventListener("click", (e) => {
-        if (e.target.classList.contains("delete-button")) {
-            const card = e.target.closest(".todo-card");
-            const id = card.dataset.id;  // Holt die 123 aus data-id="123"
-            // deleteTodo(id);              
-            console.log(`Gelöscht wird die ID: ${id}`)
-        }
-    });
+await handleTodo();
+await todoFormular();
 }
+ 
 
 main();
