9c6c44204dce50920689062f161015e586ca948b
klettenberg1970 <bastenstefan@gmailcom>
2026-05-24 16:34:32 +0200

    Update 24.05.2026

---
+++ ObsidianLinks/obsidianLinks.js
@@ -0,0 +1,24 @@
+const linksId = '1Hy7BlW81IaEi1baXFn5KqhZIkGM12FX6'
+const API = 'https://backend-dashboard-1slu.onrender.com'
+const linkContainer = document.querySelector(".links-container")
+
+const getLinks = async () => {
+    const response = await fetch(`${API}/api/projekte/datei/${linksId}`);
+    const daten = await response.json();
+    return daten
+}
+
+marked.use({
+  renderer: {
+    link({href, title, text}) {
+      return `<a href="${href}" target="_blank" rel="noreferrer"> ${text}</a>`;
+    }
+  }
+});
+
+const renderLinks = async () => {
+    const alleLinks = await getLinks();
+    linkContainer.innerHTML = marked.parse(alleLinks.singleDatei);
+}
+
+renderLinks();
